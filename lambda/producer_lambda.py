be809ff9777e8d497d34f17606221d5c7d5ec87b
Moin Naik <moinnaik98@gmail.com>
2026-06-11 20:06:12 +0530

    updated lambda functions

--- lambda/producer_lambda.py
+++ lambda/producer_lambda.py
@@ -3,39 +3,29 @@ import boto3
 import uuid
 import time
 
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('UrlShortener')
+sqs = boto3.client('sqs')
+
+QUEUE_URL = "https://sqs.ap-south-1.amazonaws.com/918792379419/url-shortener-queue"
 
 def lambda_handler(event, context):
 
     body = json.loads(event['body'])
+
     long_url = body['longUrl']
+    expiry_minutes = body['expiryMinutes']
 
-    # 1️⃣ CHECK IF EXISTS
-    response = table.query(
-        IndexName="longUrl-index",
-        KeyConditionExpression=boto3.dynamodb.conditions.Key('longUrl').eq(long_url)
-    )
-
-    if response['Items']:
-        existing = response['Items'][0]
-        return {
-            "statusCode": 200,
-            "body": json.dumps({
-                "shortUrl": "https://bit.moinnaik.in/" + existing['shortCode'],
-                "message": "Already exists"
-            })
-        }
-
-    # 2️⃣ CREATE NEW
     short_code = str(uuid.uuid4())[:6]
 
-    table.put_item(
-        Item={
-            'shortCode': short_code,
-            'longUrl': long_url,
-            'createdAt': str(time.time())
-        }
+    message = {
+        "shortCode": short_code,
+        "longUrl": long_url,
+        "expiryMinutes": expiry_minutes,
+        "createdAt": str(time.time())
+    }
+
+    sqs.send_message(
+        QueueUrl=QUEUE_URL,
+        MessageBody=json.dumps(message)
     )
 
     return {
--- lambda/worker_lambda.py
+++ lambda/worker_lambda.py
@@ -1,6 +1,7 @@
 import json
 import boto3
-from datetime import datetime
+import time
+from boto3.dynamodb.conditions import Key
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('UrlShortener')
@@ -12,16 +13,43 @@ def lambda_handler(event, context):
 
         short_code = body['shortCode']
         long_url = body['longUrl']
+        expiry_minutes = int(body['expiryMinutes'])
 
-        table.put_item(
-            Item={
-                'shortCode': short_code,
-                'longUrl': long_url,
-                'createdAt': str(datetime.utcnow())
-            }
+        expiry_time = int(time.time()) + (expiry_minutes * 60)
+
+        # CHECK IF longUrl EXISTS
+        response = table.query(
+            IndexName="longUrl-index",
+            KeyConditionExpression=Key('longUrl').eq(long_url)
         )
+
+        # IF EXISTS → UPDATE EXPIRY ONLY
+        if response['Items']:
+
+            existing_item = response['Items'][0]
+
+            table.update_item(
+                Key={
+                    "shortCode": existing_item["shortCode"]
+                },
+                UpdateExpression="SET expiryTime = :e",
+                ExpressionAttributeValues={
+                    ":e": expiry_time
+                }
+            )
+
+        # IF NOT EXISTS → INSERT NEW
+        else:
+            table.put_item(
+                Item={
+                    "shortCode": short_code,
+                    "longUrl": long_url,
+                    "expiryTime": expiry_time,
+                    "createdAt": body['createdAt']
+                }
+            )
 
     return {
         "statusCode": 200,
-        "body": "Processed SQS messages"
+        "body": json.dumps("Processed")
     }
